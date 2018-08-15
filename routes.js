a6041c5fe742434625b2f757a2f3e1d85745aa3e
1285done <tneubeck54@gmail.com>
2018-08-15 16:26:56 -0400

    Add pagination to search results

--- routes.js
+++ routes.js
@@ -5,6 +5,8 @@ var urlencodedParser = bodyParser.urlencoded({
 	extended: true
 });
 var fs = require("fs");
+const path = require('path');
+const url = require('url');
 
 const search_controller = require('./controllers/search.js')
 const form_controller = require('./controllers/form.js')
@@ -50,6 +52,57 @@ router.get('/fa.css', function (req, res) {
 	fs.readFile('/public/includes/fontawesome/css/all.min.css', function (err, data) {
 		res.send(data);
 	})
+})
+
+router.get('/*.*', function(req, res){
+	console.log(`${req.method} ${req.url}`);
+	// parse URL
+	const parsedUrl = url.parse(req.url);
+	// extract URL path
+	let pathname = `.${parsedUrl.pathname}`;
+	// maps file extention to MIME types
+	const mimeType = {
+	  '.ico': 'image/x-icon',
+	  '.html': 'text/html',
+	  '.js': 'text/javascript',
+	  '.json': 'application/json',
+	  '.css': 'text/css',
+	  '.png': 'image/png',
+	  '.jpg': 'image/jpeg',
+	  '.wav': 'audio/wav',
+	  '.mp3': 'audio/mpeg',
+	  '.svg': 'image/svg+xml',
+	  '.pdf': 'application/pdf',
+	  '.doc': 'application/msword',
+	  '.eot': 'appliaction/vnd.ms-fontobject',
+	  '.ttf': 'aplication/font-sfnt'
+	};
+	fs.exists(pathname, function (exist) {
+	  if(!exist) {
+		// if the file is not found, return 404
+		res.statusCode = 404;
+		res.end(`File ${pathname} not found!`);
+		return;
+	  }
+	  // if is a directory, then look for index.html
+	  if (fs.statSync(pathname).isDirectory()) {
+		pathname += '/index.html';
+	  }
+	  // read file from file system
+	  fs.readFile(pathname, function(err, data){
+		if(err){
+		  res.statusCode = 500;
+		  res.end(`Error getting the file: ${err}.`);
+		} else {
+		  // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+		  const ext = path.parse(pathname).ext;
+		  // if the file is found, set Content-type and send data
+		  res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
+		  res.end(data);
+		}
+	  });
+	});
+	
 })
 
 //Search
